b5a680f4fcac3a8624f813494ff8cf7bed23747f
NinaTrofimov <ntrofimov1@my.waketech.edu>
2024-04-27 11:41:51 -0400

    FinaProject Update

--- projectJS.js
+++ projectJS.js
@@ -3,107 +3,81 @@
 */
 $("#document").ready(function(){
 
-
     var days = ["<h2 id ='day'>Sunday</h2>","<h2 id ='day'>Monday</h2>", "<h2 id ='day'>Tuesday</h2>", "<h2 id ='day'>Wednesday</h2>","<h2 id ='day'>Thursday</h2>","<h2 id ='day'>Friday</h2>", "<h2 id ='day'>Saturday</h2>"];
     var names = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-   
+    let meals = ["breakfast", "snack", "lunch", "dinner"];
+
     for(let i = 0; i < days.length; i++){
         day = days[i];
         ema = names[i]
-        Meals = (` <label>Breakfast :</label><input id = 'breakfast-${ema}' type ="text"> <label>Snack :</label><input id = "snack-${ema}" type ="text"><label> Lunch :</label><input id = "lunch-${ema}" type ="text"><label> Dinner :</label><input id = "dinner-${ema}" type ="text">`);
+        var Meals = (`<label for = 'breakfast-${ema}'>Breakfast :</label><input id = 'breakfast-${ema}'><br><br/> <label for = "snack-${ema}">Snack :</label><input id = "snack-${ema}" type ="text"><label for = "lunch-${ema}"><br><br/> Lunch :</label><input id = "lunch-${ema}" type ="text"><br><br/><label for = "dinner-${ema}"> Dinner :</label><input id = "dinner-${ema}" type ="text">`);
         $('#mealInfo').append(`<div id="${ema}">${day}<div id = "boxes">${Meals}</div></div>`);
 
     }
+   
     $('#subButton').click(function(){
-       /* try repairing this?*/
-        /*var meals = ["breakfast","snack","lunch","dinner"]
-        for(var i =0; i < names.length; i++){
-            var daySchedule = {};
-            for (var j = 0; j < meals.length; j++){
-                var mealId = '#' + meals[j] + '-' + names[i];
-                daySchedule[meals[j]] = $(mealId).val();
-            }
-        }*/
-        var Sbreakfast = $('#breakfast-Sunday').val();
-        var Ssnack = $('#snack-Sunday').val();
-        var Slunch = $('#lunch-Sunday').val();
-        var Sdinner = $('#dinner-Sunday').val()
-
-        var Mbreakfast = $('#breakfast-Monday').val();
-        var Msnack = $('#snack-Monday').val();
-        var Mlunch = $('#lunch-Monday').val();
-        var Mdinner = $('#dinner-Monday').val()
-
-        var Tbreakfast = $('#breakfast-Tuesday').val();
-        var Tsnack = $('#snack-Tuesday').val();
-        var Tlunch = $('#lunch-Tuesday').val();
-        var Tdinner = $('#dinner-Tuesday').val()
-
-        var Wbreakfast = $('#breakfast-Wednesday').val();
-        var Wsnack = $('#snack-Wednesday').val();
-        var Wlunch = $('#lunch-Wednesday').val();
-        var Wdinner = $('#dinner-Wednesday').val()
-
-        var Wbreakfast = $('#breakfast-Wednesday').val();
-        var Wsnack = $('#snack-Wednesday').val();
-        var Wlunch = $('#lunch-Wednesday').val();
-        var Wdinner = $('#dinner-Wednesday').val()
-
-        var THbreakfast = $('#breakfast-Thursday').val();
-        var THsnack = $('#snack-Thursday').val();
-        var THlunch = $('#lunch-Thursday').val();
-        var THdinner = $('#dinner-Thursday').val()
-
-        var Fbreakfast = $('#breakfast-Friday').val();
-        var Fsnack = $('#snack-Friday').val();
-        var Flunch = $('#lunch-Friday').val();
-        var Fdinner = $('#dinner-Friday').val()
-
-        var SAbreakfast = $('#breakfast-Friday').val();
-        var SAsnack = $('#snack-Friday').val();
-        var SAlunch = $('#lunch-Friday').val();
-        var SAdinner = $('#dinner-Friday').val()
-
-        let info = {
-            "Days":{
-            "Sunday":{ "breakfast": Sbreakfast,
-                        "snack": Ssnack,
-                        "lunch": Slunch,
-                        "dinner": Sdinner
-            },
-            "Monday":{ "breakfast": Mbreakfast,
-                        "snack": Msnack,
-                        "lunch": Mlunch,
-                        "dinner": Mdinner
-            },
-            "Tuesday":{ "breakfast": Tbreakfast,
-                        "snack": Tsnack,
-                        "lunch": Tlunch,
-                        "dinner": Tdinner
-            },
-            "Wednesday":{ "breakfast": Wbreakfast,
-                        "snack": Wsnack,
-                        "lunch": Wlunch,
-                        "dinner": Wdinner
-            },
-            "Thursday":{ "breakfast": THbreakfast,
-                        "snack": THsnack,
-                        "lunch": THlunch,
-                        "dinner": THdinner
-            },
-            "Friday":{ "breakfast": Fbreakfast,
-                        "snack": Fsnack,
-                        "lunch": Flunch,
-                        "dinner": Fdinner
-            },
-            "Saturday":{ "breakfast": SAbreakfast,
-                        "snack": SAsnack,
-                        "lunch": SAlunch,
-                        "dinner": SAdinner
-            },
-            }
-    }
-        console.log(info);
-    })
+        /* Foods */
+        let infoArray = [];
     
-})
+        for (let day of names) {
+            let dayInfo = {};
+            for (let meal of meals) {
+                let mealValue = $(`#${meal}-${day}`).val();
+                dayInfo[meal] = mealValue;
+            }
+            infoArray.push({ [day]: dayInfo });
+        }
+    
+        let info = {
+            "Days": infoArray
+        };
+        let paragraph = ''
+        info.Days.forEach(function(day) {
+            for (let meal in day[Object.keys(day)[0]]) {
+                paragraph += `<h3>${Object.keys(day)[0]} ${meal}: ${day[Object.keys(day)[0]][meal]}<h3>`;
+            }
+            
+        })
+            /*Email check*/
+        var emailcheck = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+        var userName = $('#name').val();
+        var email = $('#email').val();
+        var goals = $('#goals').val();
+        let userInfo = {
+            "email" : email,
+            "name" : userName,
+            "goals" : goals
+        }
+        if(emailcheck.test(email)){
+            const flyWindow = window.open('about:blank','myPop','width=700,height=500,left=200,top=200');
+            let text = `<h3>Hello ${userInfo.name}, here is your meal plan!</h3>
+            <h3>Email : ${userInfo.email}</h3>
+            <h3> Goals : ${userInfo.goals}</h3>
+            `;
+            
+            flyWindow.document.write(text);
+            flyWindow.document.write(paragraph);
+            
+
+        }
+        else{
+            window.alert("Email format is not correct.")
+        }
+     });
+     $('#resetMeals').click(function(){
+        $('#mealInfo').find("input").each(function(){
+            $(this).val("");
+        });
+     })
+
+     $('#resetInfo').click(function(){
+        $('#infoInputs').find("input").each(function(){
+            $(this).val("");
+        });
+     })
+
+     $('#print').click(function(){
+        window.print();
+     })
+    });
+
